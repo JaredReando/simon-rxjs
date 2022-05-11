659f4ccd403632b0c13831ada35c3b3f15dbd6cd
Jared Reando <43949038+JaredReando@users.noreply.github.com>
2022-05-11 10:12:17 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Simon</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div className="board">
+        <div className="">Yellow</div>
+        <div className="">Blue</div>
+        <div className="">Red</div>
+        <div className="">Green</div>
+        <div className="">Simon</div>
+      </div>
+    </div>
+  );
+}
